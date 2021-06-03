9aa912616ad6dc6209dec5c517990d1d415adb7f
janhr <xhrcan@stuba.sk>
2021-06-03 11:28:21 +0200

    Final

--- src/main/java/sk/stuba/fei/uim/oop/Skuska.java
+++ src/main/java/sk/stuba/fei/uim/oop/Skuska.java
@@ -1,9 +1,7 @@
 package sk.stuba.fei.uim.oop;
 
-import java.awt.*;
-
 public class Skuska {
     public static void main(String []args){
-        Window window = new Window("Hrcan Jan Pokus");
+        new Window("Hrcan Jan Pokus");
     }
 }
